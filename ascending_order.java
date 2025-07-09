df0bba4c9050d3301d50a695128f372337efd513
Bharat C <bharathc2406@gmail.com>
2025-07-09 19:53:41 +0530

    9-7-2025

---
+++ ascending_order.java
@@ -0,0 +1,60 @@
+// example sorting in ascending order
+
+package examples;
+
+import java.util.Scanner;
+
+public class Demo12 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the numbers ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+
+		int n;
+
+		if (a > b) {
+			n = a;
+			a = b;
+			b = n;
+		}
+		if (a > c) {
+			n = a;
+			a = c;
+			c = n;
+		}
+
+		if (a > d) {
+			n = a;
+			a = d;
+			d = n;
+		}
+
+		if (b > c) {
+			n = b;
+			b = c;
+			c = n;
+		}
+		if (b > d) {
+			n = b;
+			b = d;
+			d = n;
+		}
+
+		if (c > d) {
+			n = c;
+			c = d;
+			d = n;
+		}
+
+		System.out.println("Ascending order : " + a + " " + b + " " + c + " " + d);
+	}
+}
+
+
+
+//-------------------------------
